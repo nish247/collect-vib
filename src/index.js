76052ea2cbb516054a607043d3778a8e890a46c7
nish247 <g2123046@fun.ac.jp>
2024-02-28 12:15:39 +0900

    practice

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import VibrationApp from './collectVib';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <VibrationApp />
+    <App />
   </React.StrictMode>
 );
 
